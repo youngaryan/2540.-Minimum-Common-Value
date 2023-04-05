5f7f4c9adfced348ca2df83e054d8c20ab343bb7
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-04-05 19:39:05 +0100

    passed but again very slow

--- Solution.java
+++ Solution.java
@@ -1,27 +1,24 @@
 import java.util.ArrayList;
+import java.util.TreeSet;
 import java.util.List;
 
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        List<Integer> l = new ArrayList<>();
-        List<Integer> m = new ArrayList<>();
-        for (int j : nums1) {
-            l.add(j);
-        }
-        for (int j : nums2) {
-            m.add(j);
-        }
+        TreeSet<Integer> list = new TreeSet<>();
+        TreeSet<Integer> list1 = new TreeSet<>();
+        List<Integer> list2 = new ArrayList<>();
 
-        if (l.size()>m.size())
-            for (int i = 0; i < l.size(); i++) {
-                if (l.contains(m.get(i)))
-                    return m.get(i);
-            }
-        else{
-            for (int i = 0; i < m.size(); i++) {
-                if (m.contains(l.get(i)))
-                    return l.get(i);
-            }
+        for (int i = 0; i < nums1.length; i++) {
+            list.add(nums1[i]);
+            list2.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            list1.add(nums2[i]);
+            list2.add(nums2[i]);
+        }
+        for (int i = 0; i < list2.size(); i++) {
+            if (list1.contains(list2.get(i)) && list.contains(list2.get(i)))
+                return list2.get(i);
         }
         return -1;
     }
